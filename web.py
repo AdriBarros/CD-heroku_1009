01fa540e3c7a9157de1c5c19bddf32c96295d6b3
AdriBarros <89995337+AdriBarros@users.noreply.github.com>
2022-09-27 17:52:13 -0300

    Update web.py

--- web.py
+++ web.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Olá Isaías! Tudo bem com você?"
+    return "Olá Isaías! Tudo bem com você? Esta é apenas o início de uma aprendizagem para em um futuro não muito distante eu aprender a desenvolver APIs"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
